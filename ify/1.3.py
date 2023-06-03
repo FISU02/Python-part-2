6edd4fefadce5e1b0c157e668efd0a5deec28b70
FISU02 <119970288+FISU02@users.noreply.github.com>
2023-06-03 15:33:47 +0200

    1.3.py

--- ify/1.3.py
+++ ify/1.3.py
@@ -1,25 +1,16 @@
 import random
-
-Liczba = random.randint(0,100)
-
-trys= 0
-
-pick=int(input("Zgadnij liczbę "))
-
-while pick != Liczba:
-    
-    if (pick>Liczba):
-        print("Mniej")
-        trys += 1
-        pick=int(input("Zgadnij liczbę"))
-    elif (pick>Liczba):
-        print("Mniej")
-        trys +=1
-        pick=int(input("Zgadnij liczbę"))
-    elif():
-        print("Udało ci się")
-        print("tyle razy próbowałeś zgadnąć: "+trys)
-    
-    
-
-
+play = True
+while play == True:
+    proba = 1
+    liczba = random.randint(0, 100)
+    liczba_player = int(input("Twoja liczba: "))
+    while liczba_player != liczba:
+        if liczba_player > liczba:
+            print("liczba jest mniejsza")
+        elif liczba_player < liczba:
+            print("liczba jest większa")
+        liczba_player = int(input("Twoja liczba: "))
+        proba+=1
+    print(f"Udało się! Zajęło ci to {proba} prób")
+    if input("Czy chcesz zagrać jeszcze raz ? wpisz Yes jęsli chcesz ") != "Yes":
+        play = False
